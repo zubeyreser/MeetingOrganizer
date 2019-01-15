844ed8f9a5add41a07c38a81e8f23884a405ea2f
Eser Zubeyr <eserzubeyr@gmail.com>
2019-01-15 07:42:37 +0300

    first commit

---
+++ Mechsoft_Projesi/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Mechsoft_Projesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEventCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (DatabaseEntities dc = new DatabaseEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Events e)
+        {
+            var status = false;
+            using (DatabaseEntities dc = new DatabaseEntities())
+            {
+                if (e.EventID > 0)
+                {
+                 dc.Entry(e).State = System.Data.Entity.EntityState.Modified;
+                    
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+
+                dc.SaveChanges();
+                status = true;
+
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (DatabaseEntities dc = new DatabaseEntities())
+            {
+                var v = dc.Events.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status} };
+        }
+    }
+}
---
+++ Mechsoft_Projesi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mechsoft_Projesi.MvcApplication" Language="C#" %>
